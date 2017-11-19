b1e70c22b3f0c4ec3bf237a28c412580cc92d2aa
tommy <b03705012@ntu.edu.tw>
2017-11-19 22:59:29 +0800

    fix training bugs

--- hw2/model_seq2seq.py
+++ hw2/model_seq2seq.py
@@ -134,47 +134,39 @@ def get_model(name,max_len,n_class):
 
     if not (os.path.exists("model/")):
         os.makedirs("model/")
-    if os.path.isfile("model/%s.json" % name):
-        model = model_from_json(json.load(open("model/%s.json" % name)))
-        model.compile(loss='categorical_crossentropy',
-                      optimizer='adam',
-                      metrics=['accuracy'])
-        if os.path.isfile("model/%s_model_weight.hdf5" % name):
-            model.load_weights("model/%s_model_weight.hdf5" % name)
-        print('Model loaded.')
-    else :
-        print('Model Building.')
+    
+    print('Model Building.')
 
-        video_input = keras.layers.Input([80,4096])
-        decoder_input = keras.layers.Input(shape=(max_len, nb_words + 1))
+    video_input = keras.layers.Input([80,4096])
+    decoder_input = keras.layers.Input(shape=(max_len, nb_words + 1))
 
-        encoder_output, state_h, state_c = keras.layers.LSTM(latent_dim, return_state=True, return_sequences=True)(video_input)
-        encoder_state = [state_h, state_c]
+    encoder_output, state_h, state_c = keras.layers.LSTM(latent_dim, return_state=True, return_sequences=True)(video_input)
+    encoder_state = [state_h, state_c]
 
-        hidden = keras.layers.Permute((2,1))(encoder_output)
-        hidden = keras.layers.Dense(max_len, activation='relu')(hidden)
-        hidden = keras.layers.Permute((2,1))(hidden)
+    hidden = keras.layers.Permute((2,1))(encoder_output)
+    hidden = keras.layers.Dense(max_len, activation='relu')(hidden)
+    hidden = keras.layers.Permute((2,1))(hidden)
 
-        attention = keras.layers.Dense(latent_dim, activation='softmax')(hidden)
+    attention = keras.layers.Dense(latent_dim, activation='softmax')(hidden)
 
-        hidden = keras.layers.Concatenate()([hidden, attention])
-        hidden = keras.layers.Dense(latent_dim, activation='relu')(hidden)
+    hidden = keras.layers.Concatenate()([hidden, attention])
+    hidden = keras.layers.Dense(latent_dim, activation='relu')(hidden)
 
-        video_decoder = keras.layers.LSTM(latent_dim,activation='relu',return_sequences=True)(hidden)
+    video_decoder = keras.layers.LSTM(latent_dim,activation='relu',return_sequences=True)(hidden)
 
-        decoder_output, _, _ = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)(decoder_input,initial_state=encoder_state)
+    decoder_output, _, _ = keras.layers.LSTM(latent_dim, return_sequences=True, return_state=True)(decoder_input,initial_state=encoder_state)
 
-        decoder_dense = keras.layers.Concatenate()([video_decoder,decoder_output])
+    decoder_dense = keras.layers.Concatenate()([video_decoder,decoder_output])
 
-        result = keras.layers.Dense(nb_words + 1, activation='softmax')(decoder_dense)
+    result = keras.layers.Dense(nb_words + 1, activation='softmax')(decoder_dense)
 
-        model = kmodels.Model([video_input, decoder_input], result)
+    model = kmodels.Model([video_input, decoder_input], result)
 
-        model.compile(loss='categorical_crossentropy',
-                      optimizer='adam',
-                      metrics=['accuracy'])
-        with open("model/%s.json" % model_name, 'w') as f:
-            json.dump(model.to_json(), f)
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    with open("model/%s.json" % model_name, 'w') as f:
+        json.dump(model.to_json(), f)
 
     model.summary()
     earlystopping = EarlyStopping(monitor='val_loss', patience = 5, verbose=1, mode='min')
